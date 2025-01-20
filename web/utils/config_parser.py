3780e1ed47d538fd439b61a10e9f1ba68dac2ff0
Aman vishwakarma <151424799+Botsthe@users.noreply.github.com>
2025-01-20 21:14:55 +0530

    AVBOTz 2.6

---
+++ web/utils/config_parser.py
@@ -0,0 +1,26 @@
+from os import environ
+from typing import Dict, Optional
+
+#Dont Remove My Credit @AV_BOTz_UPDATE 
+#This Repo Is By @BOT_OWNER26 
+# For Any Kind Of Error Ask Us In Support Group @AV_SUPPORT_GROUP
+
+class TokenParser:
+    def __init__(self, config_file: Optional[str] = None):
+        self.tokens = {}
+        self.config_file = config_file
+
+    def parse_from_env(self) -> Dict[int, str]:
+        self.tokens = dict(
+            (c + 1, t)
+            for c, (_, t) in enumerate(
+                filter(
+                    lambda n: n[0].startswith("MULTI_TOKEN"), sorted(environ.items())
+                )
+            )
+        )
+        return self.tokens
+
+#Dont Remove My Credit @AV_BOTz_UPDATE 
+#This Repo Is By @BOT_OWNER26 
+# For Any Kind Of Error Ask Us In Support Group @AV_SUPPORT_GROUP
---
+++ web/utils/render_template.py
@@ -0,0 +1,52 @@
+import jinja2
+from info import *
+from web.server import Webavbot
+from utils import get_size
+from web.utils.file_properties import get_file_ids
+from web.server.exceptions import InvalidHash
+import urllib.parse
+import logging
+import aiohttp
+
+#Dont Remove My Credit @AV_BOTz_UPDATE 
+#This Repo Is By @BOT_OWNER26 
+# For Any Kind Of Error Ask Us In Support Group @AV_SUPPORT_GROUP
+
+async def render_page(id, secure_hash, src=None):
+    file = await Webavbot.get_messages(int(BIN_CHANNEL), int(id))
+    file_data = await get_file_ids(Webavbot, int(BIN_CHANNEL), int(id))
+    if file_data.unique_id[:6] != secure_hash:
+        logging.debug(f"link hash: {secure_hash} - {file_data.unique_id[:6]}")
+        logging.debug(f"Invalid hash for message with - ID {id}")
+        raise InvalidHash
+
+    src = urllib.parse.urljoin(
+        URL,
+        f"{id}?hash={secure_hash}",
+    )
+
+    tag = file_data.mime_type.split("/")[0].strip()
+    file_size = get_size(file_data.file_size)
+    if tag in ["video", "audio"]:
+        template_file = "web/template/req.html"
+    else:
+        template_file = "web/template/dl.html"
+        async with aiohttp.ClientSession() as s:
+            async with s.get(src) as u:
+                file_size = get_size(int(u.headers.get("Content-Length")))
+
+    with open(template_file) as f:
+        template = jinja2.Template(f.read())
+
+    file_name = file_data.file_name.replace("_", " ")
+
+    return template.render(
+        file_name=file_name,
+        file_url=src,
+        file_size=file_size,
+        file_unique_id=file_data.unique_id,
+    )
+
+#Dont Remove My Credit @AV_BOTz_UPDATE 
+#This Repo Is By @BOT_OWNER26 
+# For Any Kind Of Error Ask Us In Support Group @AV_SUPPORT_GROUP
